fd2c9720e48f402a1a1a611711bf9e7b7a1c6432
Eduardo Silva Navarrete <enavarrete@beeldengeluid.nl>
2019-11-27 16:20:56 +0100

    update code

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Input from "../presentational/Input.jsx";
 class FormContainer extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             seo_title: ""
         };
     }
     handleChange = (event) => {
+        //http://www.qc4blog.com/?p=650
+        // diff bet' target and currentTarget
+        // console.log(event.currentTarget.value);
+        console.log(event.target.value)
         this.setState({ [event.target.id]: event.target.value });
-    }
+    };
     render() {
         const { seo_title } = this.state;
         return (
             <form id="article-form">
                 <Input
-                    text="SEO title 2"
+                    text="SEO title"
                     label="seo_title"
                     type="text"
                     id="seo_title"
